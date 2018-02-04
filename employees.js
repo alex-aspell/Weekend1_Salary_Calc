b08ecd9886456d836d3a63106382b2599eb6355a
Alex Aspell <alexgaspell@gmail.com>
2018-02-04 15:29:29 -0600

    Employee Input complete

---
+++ employees.js
@@ -0,0 +1,16 @@
+var employeeArray = [];
+
+$(document).ready(function(){
+  $('#inputNewEmployee').on('click', function(){
+    console.log('input new employee')
+
+    var newEmployee = {
+      firstName: $('#firstNameInput').val(),
+      lastName: $('#lastNameInput').val(),
+      idNumber: $('#idNumberInput').val(),
+      jobTitle: $('#jobTitleInput').val(),
+      annualSalary: $('#annualSalaryInput').val(),
+    };
+    employeeArray.push(newEmployee);
+  ;})
+})
