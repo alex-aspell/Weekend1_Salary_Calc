14c6f732f8b52c99d05409e5307309397710e552
Alexander Aspell <alexanderaspell@MacBook-Pro.zyxel.com>
2018-02-26 19:37:54 -0600

    added a 5

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ let app = express();
 
 app.use(express.static('server/public'));
 
-const port = 1234;
+const port = 12345;
 
 app.listen(port, function(){
   console.log(`Server listening on port ${port}`);
